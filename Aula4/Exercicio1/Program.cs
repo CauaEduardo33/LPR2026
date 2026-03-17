13688e2ad73b63085a4f5a42e2815e601723f96c
CauaEduardo33 <caua.eduardo@edu.etefmc.com.br>
2026-03-17 08:16:42 -0300

    Comitt do exercício 1 da aula 4

---
+++ Aula4/Exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Insira um número: ");
+int num1;
+while(!int.TryParse(Console.ReadLine(), out num1))
+{
+  Console.WriteLine("insira número inteiro: "); 
+}
+
+if(num1 % 2 == 0)
+{
+    Console.WriteLine("O número é par.");
+}
+
+else
+{
+    Console.WriteLine("O número é ímpar");
+}
+
